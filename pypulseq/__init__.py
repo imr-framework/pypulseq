95071a7da2513cb09eaceb9a3f3d2680f8117ac4
Keerthi Sravan Ravi <ks3621@columbia.edu>
2020-06-16 17:07:47 +0530

    Prepare for release 1.2.0.post2

--- pypulseq/__init__.py
+++ pypulseq/__init__.py
@@ -1,28 +1,26 @@
-name = 'pypulseq'
-__version__ = '1.2.0post1'
-__all__ = ['Sequence',
-           'add_gradients',
-           'add_ramps',
-           'align',
-           'calc_duration',
-           'calc_ramp',
-           'calc_rf_center',
-           'check_timing',
-           'compress_shape',
-           'convert',
-           'decompress_shape',
-           'event_lib',
-           'make_adc',
-           'make_arbitrary_grad',
-           'make_arbitrary_rf',
-           'make_block_pulse',
-           'make_delay',
-           'make_extended_trapezoid',
-           'make_gauss_pulse',
-           'make_sinc_pulse',
-           'make_trap_pulse',
-           'opts',
-           'points_to_waveform',
-           'split_gradient',
-           'split_gradient_at',
-           'traj_to_grad']
+from pypulseq.Sequence.sequence import Sequence
+from pypulseq.add_gradients import add_gradients
+from pypulseq.add_ramps import add_ramps
+from pypulseq.align import align
+from pypulseq.calc_duration import calc_duration
+from pypulseq.calc_ramp import calc_ramp
+from pypulseq.calc_rf_center import calc_rf_center
+from pypulseq.check_timing import check_timing
+from pypulseq.compress_shape import compress_shape
+from pypulseq.decompress_shape import decompress_shape
+from pypulseq.event_lib import EventLibrary
+from pypulseq.make_adc import make_adc
+from pypulseq.make_arbitrary_grad import make_arbitrary_grad
+from pypulseq.make_arbitrary_rf import make_trapezoid
+from pypulseq.make_block_pulse import make_block_pulse
+from pypulseq.make_delay import make_delay
+from pypulseq.make_extended_trapezoid import make_arbitrary_grad
+from pypulseq.make_gauss_pulse import make_gauss_pulse
+from pypulseq.make_sinc_pulse import make_sinc_pulse
+from pypulseq.make_trap_pulse import make_trapezoid
+from pypulseq.opts import Opts
+from pypulseq.points_to_waveform import points_to_waveform
+from pypulseq.split_gradient_at import split_gradient_at
+from pypulseq.utils.SAR.SAR_calc import calc_SAR
+from pypulseq.utils.k2g import k2g
+from pypulseq.utils.vds_2d import vds_2d
--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
 import setuptools
 
-import pypulseq
-
 try:  # Unicode decode error on Windows
     with open("README.md", "r") as fh:
         long_description = fh.read()
 except:
-    long_description = 'Pulseq on Python'
+    long_description = 'Pulseq in Python'
 
 setuptools.setup(
     name="pypulseq",
-    version=pypulseq.__version__,
+    version="1.2.0.post2",
     author="Keerthi Sravan Ravi",
     author_email="ks3621@columbia.edu",
     description="Pulseq in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/imr-framework/pypulseq",
-    packages=setuptools.find_packages(exclude=['pypulseq.utils.SAR', 'pypulseq.seq_examples']),
-    install_requires=['numpy>=1.16.3', 'matplotlib>=3.0.3'],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={'pypulseq.utils.SAR': ['QGlobal.mat']},
+    install_requires=['matplotlib>=3.0.3',
+                      'numpy>=1.16.3',
+                      'scipy>=1.4.1'],
     license='License :: OSI Approved :: GNU Affero General Public License v3',
     classifiers=[
         "Programming Language :: Python :: 3.6",
