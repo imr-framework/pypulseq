imr-framework/pypulseq

commit 11124f9ccc64f0507824e1783d9bb86bcc966fd7

🔨 Support non-int revision numbers (future releases compat.)

pypulseq/Sequence/read_seq.py
@@ -224,9 +224,9 @@ def __read_version(input_file) -> Tuple[int, int, int]:
         elif tok[0] == 'minor':
             minor = int(tok[1])
         elif tok[0] == 'revision':
-            revision = int(tok[1])
+            revision = tok[1]
         else:
-            raise RuntimeError()
+            raise RuntimeError(f'Incompatible version. Expected: {major}{minor}{revision}')
         line = __strip_line(input_file)
 
     return major, minor, revision

pypulseq/Sequence/sequence.py
@@ -27,9 +27,7 @@ from pypulseq.supported_labels import get_supported_labels
 class Sequence:
     version_major: int = major
     version_minor: int = minor
-    version_revision: int = revision
-    if isinstance(version_revision, str):
-        version_revision = int(version_revision[0])
+    version_revision = revision
 
     def __init__(self, system=Opts()):
         # =========

pypulseq/__init__.py
@@ -2,8 +2,10 @@ from pathlib import Path
 
 path_version = Path(__file__).parent.parent / 'VERSION'
 with open(str(path_version), 'r') as version_file:
-    version = version_file.read().strip().split('.')
-    major, minor, revision = [int(v) for v in version]
+    major, minor, revision = version_file.read().strip().split('.')
+    major = int(major)
+    minor = int(minor)
+
 
 from pypulseq.SAR.SAR_calc import calc_SAR
 from pypulseq.Sequence.sequence import Sequence

setup.py
@@ -1,9 +1,9 @@
-from typing import Tuple
+from typing import Tuple, Union
 
 import setuptools
 
 
-def _get_version() -> Tuple[int, int, int]:
+def _get_version() -> Tuple[int, int, Union[int, str]]:
     """
     Returns version of current PyPulseq release.
 

commit 12bd6606ad79d98869286f93fcd59fdfb19f9a1b

🐛Fix support for non-int revision numbers (see description)
Fix bug introduced in 11124f9ccc64f0507824e1783d9bb86bcc966fd7 - maintain PyPulseq version as `x.y.z` when writing `.seq flies`. Version `x.y.zpostn` is only referenced in `setup.py`.

pypulseq/Sequence/read_seq.py
@@ -224,7 +224,7 @@ def __read_version(input_file) -> Tuple[int, int, int]:
         elif tok[0] == 'minor':
             minor = int(tok[1])
         elif tok[0] == 'revision':
-            revision = tok[1]
+            revision = int(tok[1])
         else:
             raise RuntimeError(f'Incompatible version. Expected: {major}{minor}{revision}')
         line = __strip_line(input_file)

pypulseq/Sequence/sequence.py
@@ -27,7 +27,7 @@ from pypulseq.supported_labels import get_supported_labels
 class Sequence:
     version_major: int = major
     version_minor: int = minor
-    version_revision = revision
+    version_revision: int = revision
 
     def __init__(self, system=Opts()):
         # =========

pypulseq/__init__.py
@@ -7,6 +7,8 @@ with open(str(path_version), 'r') as version_file:
     major, minor, revision = version_file.read().strip().split('.')
     major = int(major)
     minor = int(minor)
+    if len(revision) > 1:
+        revision = int(revision[0])
 
 
 # =========

setup.py
@@ -1,9 +1,9 @@
-from typing import Tuple, Union
+from typing import Tuple
 
 import setuptools
 
 
-def _get_version() -> Tuple[int, int, Union[int, str]]:
+def _get_version() -> Tuple[int, int, int]:
     """
     Returns version of current PyPulseq release.
 