60ba09eb2f9e1fbe9287fb684159717e1e18b254
Bilal Taşdelen <tasdelen@usc.edu>
2022-08-10 14:02:44 -0700

    Apply requested reviews.

--- pypulseq/Sequence/sequence.py
+++ pypulseq/Sequence/sequence.py
@@ -624,7 +624,7 @@ class Sequence:
         gy = points_to_waveform(times=ty, amplitudes=ws[1][1], grad_raster_time=dt)
         gz = points_to_waveform(times=tz, amplitudes=ws[2][1], grad_raster_time=dt)
 
-        # Check for empty arrays, end set end and starting times
+        # Check for empty arrays, set end and starting times
         tx_s = 0 if tx.size==0 else tx[0]
         ty_s = 0 if ty.size==0 else ty[0]
         tz_s = 0 if tz.size==0 else tz[0]
@@ -633,15 +633,14 @@ class Sequence:
         ty_e = 0 if ty.size==0 else ty[-1]
         tz_e = 0 if tz.size==0 else tz[-1]
 
-        max_t = round(max(tx_e, ty_e, tz_e)/dt)*dt
+        max_t = np.round(np.max(tx_e, ty_e, tz_e)/dt)*dt
 
-
-        gx_i = round((tx_s)/dt)
-        gy_i = round((ty_s)/dt)
-        gz_i = round((tz_s)/dt)
+        gx_i = np.round((tx_s)/dt)
+        gy_i = np.round((ty_s)/dt)
+        gz_i = np.round((tz_s)/dt)
 
         # Create array for waveforms and insert into respective axes
-        grad_waveforms = np.zeros((3, round(max_t/dt)))
+        grad_waveforms = np.zeros((3, np.round(max_t/dt)))
 
         grad_waveforms[0, gx_i:(gx_i+gx.shape[0])] = gx
         grad_waveforms[1, gy_i:(gy_i+gy.shape[0])] = gy
