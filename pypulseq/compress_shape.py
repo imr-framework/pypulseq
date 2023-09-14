7be9182d5d3cdca340fc63792c77c3c5dc6bec63
Frank Zijlstra <22915457+FrankZijlstra@users.noreply.github.com>
2023-09-14 14:50:38 +0200

    Minor performance improvement of `compress_shape` using np.repeat

--- pypulseq/compress_shape.py
+++ pypulseq/compress_shape.py
@@ -41,28 +41,25 @@ def compress_shape(
     quant_factor = 1e-7
     decompressed_shape_scaled = decompressed_shape / quant_factor
     datq = np.round(
-        np.insert(np.diff(decompressed_shape_scaled), 0, decompressed_shape_scaled[0])
+        np.concatenate((decompressed_shape_scaled[[0]], np.diff(decompressed_shape_scaled)))
     )
     qerr = decompressed_shape_scaled - np.cumsum(datq)
-    qcor = np.insert(np.diff(np.round(qerr)), 0, 0)
+    qcor = np.concatenate(([0], np.diff(np.round(qerr))))
     datd = datq + qcor
 
-    mask_changes = np.insert(np.asarray(np.diff(datd) != 0, dtype=np.int32), 0, 1)
-    # Elements without repetitions
-    vals = datd[mask_changes.nonzero()[0]] * quant_factor
-
-    # Indices of changes
-    k = np.append(mask_changes, 1).nonzero()[0]
-    # Number of repetitions
-    n = np.diff(k)
-
-    n_extra = (n - 2).astype(np.float32)  # Cast as float for nan assignment to work
-    vals2 = np.copy(vals)
-    vals2[n_extra < 0] = np.nan
-    n_extra[n_extra < 0] = np.nan
-    v = np.stack((vals, vals2, n_extra))
-    v = v.T[np.isfinite(v).T]  # Use transposes to match Matlab's Fortran indexing order
-    v[abs(v) < 1e-10] = 0
+    # RLE of datd
+    starts = np.concatenate(([0], np.flatnonzero(datd[1:] != datd[:-1])+1))
+    lengths = np.diff(np.concatenate((starts, [len(datd)])))
+    values = datd[starts] * quant_factor
+    
+    # Repeat values of any run-length>1 three times: (value, value, length)
+    rl_gt1 = lengths>1
+    repeats = 1 + rl_gt1*2
+    v = np.repeat(values, repeats)
+    
+    # Calculate indices of length elements and insert length values
+    inds = np.cumsum(repeats) - 1
+    v[inds[rl_gt1]] = lengths[rl_gt1] - 2
 
     compressed_shape = SimpleNamespace()
     compressed_shape.num_samples = len(decompressed_shape)
