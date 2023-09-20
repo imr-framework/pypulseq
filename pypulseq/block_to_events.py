e1ae8b9042192f0831952ba2fae6eed2d123e7b8
Frank Zijlstra <22915457+FrankZijlstra@users.noreply.github.com>
2023-09-20 11:31:01 +0200

    Cleaned up and optimized `block_to_events` - Cleaned up the check whether it is called with a list of events or a block - Prevent iterating and constructing a new list when called with a list of events (labels will never be a dict) - Use `block_to_events` in `Sequence.check_timing` to prevent labels being represented as a `dict` in calls to `calc_duration`

--- pypulseq/block_to_events.py
+++ pypulseq/block_to_events.py
@@ -16,27 +16,17 @@ def block_to_events(*args: SimpleNamespace) -> Tuple[SimpleNamespace, ...]:
     events : list[SimpleNamespace]
         List of events comprising `args` if it was a block, otherwise `args` unmodified.
     """
-    if (
-        len(args) == 1
-    ):  # args is a tuple consisting a block of events, or a single event
-        x = args[0]
-
-        if isinstance(x, (float, int)):  # args is block duration
-            events = [x]
-        else:  # args could be a block of events or a single event
-            events = list(vars(x).values())  # Get all attrs
-            events = list(
-                filter(lambda filter_none: filter_none is not None, events)
-            )  # Filter None attributes
-            # If all attrs are either float/SimpleNamespace, args is a block of events
-            if all([isinstance(e, (float, SimpleNamespace)) for e in events]):
-                events = __get_label_events_if_any(
-                    *events
-                )  # Flatten label events from dict datatype
-            else:  # Else, args is a single event
-                events = [x]
+    if len(args) == 1 and hasattr(args[0], 'rf'):
+        events = list(vars(args[0]).values())  # Get all attrs
+        events = list(
+            filter(lambda filter_none: filter_none is not None, events)
+        )  # Filter None attributes
+        events = __get_label_events_if_any(
+            *events
+        )  # Flatten label events from dict datatype
+        
     else:  # args is a tuple of events
-        events = __get_label_events_if_any(*args)
+        return args
 
     return events
 
