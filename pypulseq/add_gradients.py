29e62afa0b06e8cbaa723aa4afe770959ccd5f6a
Bilal Taşdelen <bilaltasdelen@windowslive.com>
2024-08-02 15:06:22 -0700

    Fix incorrect and non-zero delay.

--- pypulseq/add_gradients.py
+++ pypulseq/add_gradients.py
@@ -201,8 +201,7 @@ def add_gradients(
         if g.delay - common_delay > 0:
             # Stop for numpy.arange is not g.delay - common_delay - system.grad_raster_time like in Matlab
             # so as to include the endpoint
-            t_delay = np.arange(0, g.delay - common_delay, step=system.grad_raster_time)
-            waveforms[ii] = np.concatenate(([t_delay], waveforms[ii]))
+            waveforms[ii] = np.concatenate((np.zeros(round((g.delay-common_delay)/system.grad_raster_time)), waveforms[ii]))
 
         num_points = len(waveforms[ii])
         max_length = max(num_points, max_length)
