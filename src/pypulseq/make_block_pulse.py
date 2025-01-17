4a25069569e863d110ac775bd5e65d1a25a99ea2
Helge Herthum <115626510+h3lg3@users.noreply.github.com>
2025-01-17 09:24:29 +0100

    Add warning to make_block_pulse() if rf.delay is changed to rf.dead_time (#236)

--- src/pypulseq/make_block_pulse.py
+++ src/pypulseq/make_block_pulse.py
@@ -109,6 +109,10 @@ def make_block_pulse(
         rf.use = use
 
     if rf.dead_time > rf.delay:
+        warn(
+            f'Specified RF delay {rf.delay*1e6:.2f} us is less than the dead time {rf.dead_time*1e6:.0f} us. Delay was increased to the dead time.',
+            stacklevel=2,
+        )
         rf.delay = rf.dead_time
 
     if trace_enabled():
