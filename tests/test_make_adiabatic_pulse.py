0101e308cfb8134e4bb800ea947fd979e56b3fc7
Markus Zimmermann <m.zimmermann@fz-juelich.de>
2025-02-20 15:36:32 +0000

    Add tests for hypsec_n pulse.

--- tests/test_make_adiabatic_pulse.py
+++ tests/test_make_adiabatic_pulse.py
@@ -13,7 +13,7 @@ from pypulseq.supported_labels_rf_use import get_supported_rf_uses
 
 def test_pulse_select():
     valid_rf_use_labels = get_supported_rf_uses()
-    valid_pulse_types = ('hypsec', 'wurst')
+    valid_pulse_types = ('hypsec', 'hypsec_n', 'wurst')
 
     # Check all use and valid pulse combinations return a sensible object
     # with default parameters.
@@ -64,6 +64,10 @@ def test_hypsec_options():
 
     assert np.isclose(pobj.shape_dur, 0.05)
 
+def test_hypsec_n_options():
+    pobj = make_adiabatic_pulse(pulse_type='hypsec_n', beta=400, mu=9.8, order=4, duration=0.01)
+
+    assert np.isclose(pobj.shape_dur, 0.01)
 
 def test_wurst_options():
     pobj = make_adiabatic_pulse(pulse_type='wurst', n_fac=25, bandwidth=30000, duration=0.05)
