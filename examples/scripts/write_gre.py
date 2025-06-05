1b2452f1e60ccaf12cf68afb4ad51acf01127c87
Helge Herthum <115626510+h3lg3@users.noreply.github.com>
2025-06-05 17:36:28 +0200

    Fixes timing of write_gre sequence. (#257)
    * fixes timing of GRE sequences if rf.ringdown_time gets larger then gz.fall_time
    
    * - fixes TE time issue without assuming that the center of the RF pulse is in the center of the waveform
    
    * - replace reference write_gre.seq due to previous rounding issue in calculation of delay_TE. delay_TE is now 81 ms instead of 82 ms.
    
    * - remove FOV and name from header of reference sequence

--- examples/scripts/write_gre.py
+++ examples/scripts/write_gre.py
@@ -59,15 +59,21 @@ def main(plot: bool = False, write_seq: bool = False, seq_filename: str = 'gre_p
 
     # Calculate timing
     delay_TE = (
-        np.ceil(
-            (TE - pp.calc_duration(gx_pre) - gz.fall_time - gz.flat_time / 2 - pp.calc_duration(gx) / 2)
+        math.ceil(
+            (
+                TE
+                - (pp.calc_duration(gz, rf) - pp.calc_rf_center(rf)[0] - rf.delay)
+                - pp.calc_duration(gx_pre)
+                - pp.calc_duration(gx) / 2
+                - pp.eps
+            )
             / seq.grad_raster_time
         )
         * seq.grad_raster_time
     )
     delay_TR = (
         np.ceil(
-            (TR - pp.calc_duration(gz) - pp.calc_duration(gx_pre) - pp.calc_duration(gx) - delay_TE)
+            (TR - pp.calc_duration(gz, rf) - pp.calc_duration(gx_pre) - pp.calc_duration(gx) - delay_TE)
             / seq.grad_raster_time
         )
         * seq.grad_raster_time
