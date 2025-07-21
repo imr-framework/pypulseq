dafebe5e6ddb8adb3af3dea8bc5f832fb2e6cca7
mcencini <matteo.cencini@gmail.com>
2025-07-21 11:50:35 +0200

    update label

--- src/pypulseq/make_label.py
+++ src/pypulseq/make_label.py
@@ -13,7 +13,7 @@ def make_label(label: str, type: str, value: Union[bool, float, int]) -> SimpleN
     type : str
         Label type. Must be one of 'SET' or 'INC'.
     label : str
-        Must be one of 'SLC', 'SEG', 'REP', 'AVG', 'SET', 'ECO', 'PHS', 'LIN', 'PAR', 'NAV', 'REV', or 'SMS'.
+        Must be one of `pypulseq.get_supported_labels()`.
     value : bool, float or int
         Label value.
 
@@ -25,18 +25,15 @@ def make_label(label: str, type: str, value: Union[bool, float, int]) -> SimpleN
     Raises
     ------
     ValueError
-        If a valid `label` was not passed. Must be one of 'SLC', 'SEG', 'REP', 'AVG', 'SET', 'ECO', 'PHS', 'LIN', 'PAR',
-                                                                                                NAV', 'REV', or 'SMS'.
+        If a valid `label` was not passed. Must be one of `pypulseq.get_supported_labels()`.
         If a valid `type` was not passed. Must be one of 'SET' or 'INC'.
         If `value` was not a valid numerical or logical value.
     """
     arr_supported_labels = get_supported_labels()
+    arr_flags = arr_supported_labels[10:-1]
 
     if label not in arr_supported_labels:
-        raise ValueError(
-            "Invalid label. Must be one of 'SLC', 'SEG', 'REP', 'AVG', 'SET', 'ECO', 'PHS', 'LIN', 'PAR', "
-            "NAV', 'REV', or 'SMS'."
-        )
+        raise ValueError(f'Invalid label. Must be one of {arr_supported_labels}.')
     if type not in ['SET', 'INC']:
         raise ValueError("Invalid type. Must be one of 'SET' or 'INC'.")
     if not isinstance(value, (bool, float, int)):
@@ -46,6 +43,8 @@ def make_label(label: str, type: str, value: Union[bool, float, int]) -> SimpleN
     if type == 'SET':
         out.type = 'labelset'
     elif type == 'INC':
+        if label in arr_flags:
+            raise ValueError(f'As per Pulseq specification, labelinc is not compatible with flags: {arr_flags}.')
         out.type = 'labelinc'
 
     out.label = label
