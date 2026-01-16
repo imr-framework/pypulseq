2d846febc21ea9c0aeb2b67a6cfc9c71393df8ed
Patrick Schuenke <patrick.schuenke@nvision-imaging.com>
2026-01-16 11:40:39 +0100

    remove ruff.toml file in SAR subfolder

--- src/pypulseq/SAR/SAR_calc.py
+++ src/pypulseq/SAR/SAR_calc.py
@@ -7,5 +7,5 @@ from pypulseq.Sequence.sequence import Sequence
 def calc_SAR(file: Union[str, Path, Sequence]) -> None:  # noqa: ARG001
     """Temporary placeholder for DeprecationError."""
     raise RuntimeError(
-        'Built-in SAR computation has been removed; use PySar4seq (https://github.com/imr-framework/sar4seq/tree/PySar4seq)'
+        'Built-in SAR computation has been removed; use PySar4seq (https://github.com/imr-framework/sar4seq/tree/PySar4seq) instead.'
     )
