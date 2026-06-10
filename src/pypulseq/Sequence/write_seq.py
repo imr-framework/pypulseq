abb4ef394765f6cc4a1574bc64b9669429194961
albertx-wang <albert-xiang.wang@connect.polyu.hk>
2026-06-10 11:20:27 +0200

    Preserve text signature output format

--- src/pypulseq/Sequence/write_seq.py
+++ src/pypulseq/Sequence/write_seq.py
@@ -263,11 +263,16 @@ def write(self, file_name: Union[str, Path], create_signature, remove_duplicates
         # Write signature
         with open(file_name, 'a') as output_file:
             output_file.write('\n[SIGNATURE]\n')
-            output_file.write('# This is the hash of the Pulseq file, calculated right before the [SIGNATURE]\n')
-            output_file.write('# section was added. It can be reproduced/verified with md5sum if the file\n')
-            output_file.write('# trimmed to the position right above [SIGNATURE]. The new line character\n')
-            output_file.write('# preceding [SIGNATURE] BELONGS to the signature (and needs to be sripped away\n')
-            output_file.write('# for recalculating/verification)\n')
+            output_file.write(
+                '# This is the hash of the Pulseq file, calculated right before the [SIGNATURE] section was added\n'
+            )
+            output_file.write(
+                '# It can be reproduced/verified with md5sum if the file trimmed to the position right above [SIGNATURE]\n'
+            )
+            output_file.write(
+                '# The new line character preceding [SIGNATURE] BELONGS to the signature (and needs to be stripped away for '
+                'recalculating/verification)\n'
+            )
             output_file.write('Type md5\n')
             output_file.write(f'Hash {md5}\n')
 
@@ -518,11 +523,16 @@ def write_v141(self, file_name: Union[str, Path], create_signature, remove_dupli
         # Write signature
         with open(file_name, 'a') as output_file:
             output_file.write('\n[SIGNATURE]\n')
-            output_file.write('# This is the hash of the Pulseq file, calculated right before the [SIGNATURE]\n')
-            output_file.write('# section was added. It can be reproduced/verified with md5sum if the file\n')
-            output_file.write('# trimmed to the position right above [SIGNATURE]. The new line character\n')
-            output_file.write('# preceding [SIGNATURE] BELONGS to the signature (and needs to be sripped away\n')
-            output_file.write('# for recalculating/verification)\n')
+            output_file.write(
+                '# This is the hash of the Pulseq file, calculated right before the [SIGNATURE] section was added\n'
+            )
+            output_file.write(
+                '# It can be reproduced/verified with md5sum if the file trimmed to the position right above [SIGNATURE]\n'
+            )
+            output_file.write(
+                '# The new line character preceding [SIGNATURE] BELONGS to the signature (and needs to be stripped away for '
+                'recalculating/verification)\n'
+            )
             output_file.write('Type md5\n')
             output_file.write(f'Hash {md5}\n')
 
